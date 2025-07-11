602735f7da386e0fa45d3e4707fc846d78e170e5
jwj4016 <jwj4016@gmail.com>
2025-07-11 17:41:13 +0900

    SimpleLoggerAdvisor 추가.

--- src/main/java/com/example/springaisample/config/AiConfig.java
+++ src/main/java/com/example/springaisample/config/AiConfig.java
@@ -2,6 +2,7 @@ package com.example.springaisample.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
@@ -24,6 +25,7 @@ public class AiConfig {
 				.defaultSystem("You are a helpful DBA who speaks like a RDB expert.")
 				.defaultAdvisors(
 						MessageChatMemoryAdvisor.builder(chatMemory()).build()
+						, new SimpleLoggerAdvisor()
 				)
 				.build();
 
